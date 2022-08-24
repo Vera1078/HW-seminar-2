8a62cffd511c3041b9b1257b630cb0edb0cb363a
Vera Lippgardt <vera1078@mail.ru>
2022-08-24 18:11:38 +0300

    Вывод второй цифры 3х-значного числа

---
+++ aufgabe10/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+while (number < 100 || number > 999)
+{
+Console.Write("Число неверное. Введите трехзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+}
+
+int num = (number / 10) % 10;
+System.Console.WriteLine ($"Вторая цифра числа = {num}");
