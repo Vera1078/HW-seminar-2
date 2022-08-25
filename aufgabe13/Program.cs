77d737b38c13406a47954017ea339f1aa6f671a4
Vera Lippgardt <vera1078@mail.ru>
2022-08-25 18:01:50 +0300

    Показать третью цифру числа

---
+++ aufgabe13/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+string str = Convert.ToString (number);
+
+if (number < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет.");
+}
+
+else
+{
+Console.WriteLine (str [2]);
+}
