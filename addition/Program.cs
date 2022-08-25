391f8868c02a3187ee2773a6ee38adfb71f847f0
Vera Lippgardt <vera1078@mail.ru>
2022-08-25 18:44:50 +0300

    Дополнительное задание

---
+++ addition/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+//  14 -> нет 46 -> нет 161 -> да
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+int div1 = number % 7;
+int div2 = number % 23;
+
+if (div1 == 0 && div2 == 0)
+{
+    System.Console.WriteLine("ДА");
+    }
+
+else
+{
+    System.Console.WriteLine("НЕТ");
+}
+
